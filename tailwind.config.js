c366dfcf65fc832afa4401800862e42c7739d278
Farhan Khalid <farhankhalid.dev@gmail.com>
2024-04-28 07:03:59 +0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templayes/customer/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
